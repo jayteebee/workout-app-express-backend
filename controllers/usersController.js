2db14a616259fcb94a1a558d5911729b084d7913
Jethro_Block <jethroblock22@gmail.COM>
2023-10-18 15:27:27 +0100

    create and export deleteOneUser action

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -58,9 +58,22 @@ const updateOneUser = async (req,res) => {
     }
 }
 
+// DELETE REQUESTS
+// Delete One User
+const deleteOneUser = async (req,res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        res.status(201).json(user)
+    }
+    catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
+
 module.exports = {
     getAllUsers,
     getOneUser,
     createOneUser,
-    updateOneUser
+    updateOneUser,
+    deleteOneUser
 }
