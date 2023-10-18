4b2599dc95eeedf4eea18909aae93e4dc87ec036
Jethro_Block <jethroblock22@gmail.COM>
2023-10-18 16:52:26 +0100

    build authenticateToken middleware

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -105,6 +105,20 @@ const deleteOneUser = async (req,res) => {
     }
 }
 
+// Middleware
+const authenticateToken = (req,res,next) => {
+    // access the authorization header in the incoming request (think: axiosInstanceWithToken)
+const authHeader = req.headers["Authorization"]
+const token = authHeader && authHeader.split(" ")[1]
+if (token == null) return res.status(401)
+
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, email) => {
+      if (err) return res.status(403)
+      req.user = email
+      next()
+})
+}
+
 module.exports = {
     getAllUsers,
     getOneUser,
