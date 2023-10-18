da814c7c1516ca9887dd3c65b12665d67a625102
Jethro_Block <jethroblock22@gmail.COM>
2023-10-18 15:07:20 +0100

    create getAllUsers route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,18 @@
 const express = require("express")
 const User = require("../schemas/userSchema")
 
+// GET REQUESTS
+// Get All
+const getAllUsers = async (req,res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    }
+    catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
 
-module.exports = {}
+module.exports = {
+    getAllUsers
+}
