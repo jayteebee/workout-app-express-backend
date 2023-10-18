81d2ff50b4be91a73dbc7b461be4b2d9e8829cc5
Jethro_Block <jethroblock22@gmail.COM>
2023-10-18 11:25:51 +0100

    Create dynamic database recognition

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require("dotenv").config()
+const cors = require("cors")
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const env = process.env.NODE_ENV || "development";
+let dbUri;
+if (env === "production") {
+  dbUri = process.env.DATABASE_URL_PROD
+} else {
+  dbUri = process.env.DATABASE_URL_DEV
+}
+
+mongoose.connect(dbUri, {useNewUrlParser: true})
 const db = mongoose.connection
 db.on("error", (err) => console.log(err))
 db.once("open", () => console.log('connected to database'))
