b45a75da19633c53cda7ba8b4d19a2e92c0b3e42
Jethro_Block <jethroblock22@gmail.COM>
2023-10-18 11:55:27 +0100

    Create user schema and modify naming conventions

---
+++ schemas/userSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    height: {
+        type: Number,
+        required: false
+    },
+    weight: {
+        type: Number,
+        required: false
+    }
+})
+
+module.exports = mongoose.model("users", userSchema, "users")
