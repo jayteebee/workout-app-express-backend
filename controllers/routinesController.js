035a3def2aa5de6b8303fbb496b8a5b6b67cf21f
Jethro_Block <jethroblock22@gmail.COM>
2023-11-14 14:23:17 +0000

    add createRoutine route

--- controllers/routinesController.js
+++ controllers/routinesController.js
@@ -12,6 +12,24 @@ const getAllRoutines = async (req,res) => {
     }
 }
 
+// POST REQUESTS
+// Create One
+const createRoutine = async (req,res) => {
+    try {
+        const routine = new Routine({
+            name: req.body.name,
+            frequency: req.body.frequency,
+            id: req.body.user_id
+        })
+        const newRoutine = await routine.save()
+        res.status(201).json(newRoutine)
+    }
+    catch (err) {
+        res.status(500).json({message: err.message})
+    }
+}
+
 module.exports = {
-    getAllRoutines
+    getAllRoutines,
+    createRoutine
 }
