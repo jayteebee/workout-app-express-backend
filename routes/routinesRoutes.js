63d7c9f6673f235c21e854235d39e3913d69b710
Jethro_Block <jethroblock22@gmail.COM>
2023-11-14 14:24:09 +0000

    add createRoutine route

--- routes/routinesRoutes.js
+++ routes/routinesRoutes.js
@@ -2,10 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 const {
-getAllRoutines
+getAllRoutines,
+createRoutine
 } = require("../controllers/routinesController")
 
 router.route("/routines")
     .get(getAllRoutines)
+
+router.route("/routines")
+    .post(createRoutine)
 
 module.exports = router
